35cd1aba8856e66261c3cca72f5f2d31ac38a183
jhansarmiento <jhancsn1@gmail.com>
2024-11-09 19:47:04 -0500

    Crea un servidor con express

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { sumar } from './server'
+import server from "./server";
 
-console.log('Desde index.ts')
-console.log('Después de nodemon')
-sumar()
+server.listen(4000, () => {
+    console.log(`REST API en el puerto 4000`)
+})
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,16 @@
-export function sumar() {
-    console.log(2 + 2)
-}
+import express from "express";
+
+const  server = express()
+
+// Routing - Crea el servidor
+server.get('/', (req, res) => {
+    
+    const datos = [
+        { id: 1, nombre: 'Jhan'},
+        { id: 2, nombre: 'Carlos'},
+
+    ]
+    res.json(datos)
+})
+
+export default server
