39683ca7ad23ba6e5a39b9b8b1bd79b007aa8808
jhansarmiento <jhancsn1@gmail.com>
2025-01-24 15:38:25 -0500

    Prueba la validacion de POST

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -2,6 +2,17 @@ import request from 'supertest'
 import server from '../../server'
 
 describe('POST /api/products', () => {
+    it('Should display errors messages', async () => {
+        const response = await request(server).post('/api/products').send({})
+
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(4)
+        
+        expect(response.status).not.toBe(201)
+        expect(response.body.errors).not.toHaveLength(2)
+    })
+    
     it('Should create a new product', async () => {
         const response = await request(server).post('/api/products').send({
             name : "Monitor Lamp",
@@ -12,6 +23,6 @@ describe('POST /api/products', () => {
         expect(response.body).toHaveProperty('data')
 
         expect(response.status).not.toBe(400)
-        expect(response.body).not.toHaveProperty('error')
+        expect(response.body).not.toHaveProperty('errors')
     })
 })
