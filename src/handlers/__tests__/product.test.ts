0d16a04a8d15365c0d7962a078680766d34561db
jhansarmiento <jhancsn1@gmail.com>
2025-01-24 16:49:48 -0500

    Valida que el precio sea mayor que 0

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 import server from '../../server'
 
 describe('POST /api/products', () => {
-    it('Should display errors messages', async () => {
+    it('Should display validation errors', async () => {
         const response = await request(server).post('/api/products').send({})
 
         expect(response.status).toBe(400)
@@ -13,6 +13,34 @@ describe('POST /api/products', () => {
         expect(response.body.errors).not.toHaveLength(2)
     })
     
+    it('Should validate that the price is greater than 0', async () => {
+        const response = await request(server).post('/api/products').send({
+            name : "Office Chair",
+            price : 0
+        })
+
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(1)
+        
+        expect(response.status).not.toBe(201)
+        expect(response.body.errors).not.toHaveLength(2)
+    })
+
+    it('Should validate that the price is greater than 0', async () => {
+        const response = await request(server).post('/api/products').send({
+            name : "Office Chair",
+            price : "Hola"
+        })
+
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(2)
+        
+        expect(response.status).not.toBe(201)
+        expect(response.body.errors).not.toHaveLength(1)
+    })
+
     it('Should create a new product', async () => {
         const response = await request(server).post('/api/products').send({
             name : "Monitor Lamp",
