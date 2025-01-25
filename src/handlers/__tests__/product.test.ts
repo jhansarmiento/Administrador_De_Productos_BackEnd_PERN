7b1bc49dc9a60f5ceddd830cb3b4db81bcb51dd4
jhansarmiento <jhancsn1@gmail.com>
2025-01-25 10:41:56 -0500

    Testea el metodo de Get para todos los productos

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -54,3 +54,21 @@ describe('POST /api/products', () => {
         expect(response.body).not.toHaveProperty('errors')
     })
 })
+
+describe('GET /api/products', () => {
+    
+    it('Checks if URL /api/products exists', async () => {
+        const response = await request(server).get('/api/products')
+        expect(response.status).not.toBe(404)
+    })
+
+    it('Gets a Json response with the products', async () => {
+        const response = await request(server).get('/api/products')
+
+        expect(response.status).toBe(200)
+        expect(response.headers['content-type']).toMatch(/json/)
+        expect(response.body).toHaveProperty('data')
+        expect(response.body.data).toHaveLength(1)
+        expect(response.body).not.toHaveProperty('errors')
+    })
+})
