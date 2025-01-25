0905076b1188b90c4e4aaa12e1d78d3977c04ff5
jhansarmiento <jhancsn1@gmail.com>
2025-01-25 11:57:23 -0500

    Testea el metodo put para actualizar un producto

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -74,6 +74,15 @@ describe('GET /api/products', () => {
 })
 
 describe('GET /api/products/:id', () => {
+    it('Should check a valid ID in the URL', async () => {
+        const response = await request(server).get(`/api/products/not-valid-url`)
+
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(1)
+        expect(response.body.errors[0].msg).toBe('Not Valid ID')
+    })
+
     it('Should return a 404 for a non existing product', async () => {
         const productID = 2000
         const response = await request(server).get(`/api/products/${productID}`)
@@ -83,19 +92,91 @@ describe('GET /api/products/:id', () => {
         expect(response.body.error).toBe('Product Not Found')
     })
     
-    it('Should check a valid ID in the URL', async () => {
-        const response = await request(server).get(`/api/products/not-valid-url`)
-
-        expect(response.status).toBe(400)
-        expect(response.body).toHaveProperty('errors')
-        expect(response.body.errors).toHaveLength(1)
-        expect(response.body.errors[0].msg).toBe('Not Valid ID')
-    })
     
     it('Gets a JSON response for a single product', async () => {
         const response = await request(server).get(`/api/products/1`)
 
         expect(response.status).toBe(200)
         expect(response.body).toHaveProperty('data')
+    })
+})
+
+describe('PUT /api/products/:id', () => {
+
+    it('Should check a valid ID in the URL', async () => {
+        const response = await request(server)
+                                .put(`/api/products/not-valid-url`)
+                                .send({
+                                    name : "Office Chair",
+                                    price: 300,
+                                    availability: true
+                                })
+
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(1)
+        expect(response.body.errors[0].msg).toBe('Not Valid ID')
+    })
+
+    it('Should display validation errors messages when updating a product', async () => {
+        const response = await request(server).put('/api/products/1').send({})
+
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(5)
+        
+        expect(response.status).not.toBe(201)
+        expect(response.body).not.toHaveProperty('data')
+    })
+    
+    it('Should validate that the price is greater than 0', async () => {
+        const response = await request(server)
+                                .put('/api/products/1')
+                                .send({
+                                    name : "Office Chair",
+                                    price: 0,
+                                    availability: true
+                                })
+
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors[0].msg).toBe("Not valid value")
+        expect(response.body.errors).toHaveLength(1)
+        
+        expect(response.status).not.toBe(200)
+        expect(response.body).not.toHaveProperty('data')
+    })
+    
+    it('Should return a 404 response for a non-existing product', async () => {
+        const productID = 2000
+        const response = await request(server)
+                                .put(`/api/products/${productID}`)
+                                .send({
+                                    name : "Office Chair",
+                                    price: 300,
+                                    availability: true
+                                })
+
+        expect(response.status).toBe(404)
+        expect(response.body.error).toBe("Product Not Found")
+        
+        expect(response.status).not.toBe(200)
+        expect(response.body).not.toHaveProperty('data')
+    })
+
+    it('Should update an existing product with valid data', async () => {
+        const response = await request(server)
+                                .put("/api/products/1")
+                                .send({
+                                    name : "Office Chair",
+                                    price: 300,
+                                    availability: true
+                                })
+
+        expect(response.status).toBe(200)
+        expect(response.body).toHaveProperty('data')
+        
+        expect(response.status).not.toBe(404)
+        expect(response.body).not.toHaveProperty('errors')
     })
 })
