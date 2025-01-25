1ddbb5f195ba95ab2a28ee1b318dc9d7c59ddebe
jhansarmiento <jhancsn1@gmail.com>
2025-01-25 16:42:00 -0500

    Testea el endpoint de Delete

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest'
 import server from '../../server'
+import { response } from 'express'
 
 describe('POST /api/products', () => {
     it('Should display validation errors', async () => {
@@ -178,5 +179,35 @@ describe('PUT /api/products/:id', () => {
         
         expect(response.status).not.toBe(404)
         expect(response.body).not.toHaveProperty('errors')
+    })
+})
+
+describe('DELETE /api/products/:id', () => {
+    it('Should check a valid ID', async () => {
+        const response =  await request(server).delete('/api/products/not-valid-url')
+
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors[0].msg).toBe('Not Valid ID')
+    })
+    
+    it('Should return a 404 response for a non-existing product', async () => {
+        const productID = 2000
+        const response = await request(server).delete(`/api/products/${productID}`)
+        
+        expect(response.status).toBe(404)
+        expect(response.body.error).toBe('Product Not Found')
+
+        expect(response.status).not.toBe(200)
+    })
+
+    it('Should delete a product', async () => {
+        const response = await request(server).delete('/api/products/1')
+
+        expect(response.status).toBe(200)
+        expect(response.body.data).toBe('Product Deleted')
+
+        expect(response.status).not.toBe(404)
+        expect(response.status).not.toBe(400)
     })
 })
