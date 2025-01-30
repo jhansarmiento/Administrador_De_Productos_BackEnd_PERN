bcdb3c7613191b893aa2056d3a4c66336c5775d2
jhansarmiento <jhancsn1@gmail.com>
2025-01-30 00:46:10 -0500

    Personaliza la barra superior de la documentacion

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -1,4 +1,5 @@
 import swaggerJSDoc from 'swagger-jsdoc'
+import { SwaggerUiOptions } from 'swagger-ui-express'
 
 const options : swaggerJSDoc.Options = {
     swaggerDefinition: {
@@ -20,4 +21,24 @@ const options : swaggerJSDoc.Options = {
  
 const swaggerSpec = swaggerJSDoc(options)
 
+const swaggerUiOptions : SwaggerUiOptions = {
+    customCss: `
+        /* Añadir texto después del logo */
+        .topbar-wrapper::after {
+            content: "REST API - Node.js / Express / TypeScript";
+            margin-left: 20px;
+            color: white; 
+            font-size: 1.2em;
+        }
+        .swagger-ui .topbar {
+            background-color: #1a1a1a; /* Color de fondo oscuro */
+            padding: 10px 0;
+        }
+    `,
+    customSiteTitle: 'Documentación De Administrador De Productos'
+}
+
 export default swaggerSpec
+export {
+    swaggerUiOptions
+}
